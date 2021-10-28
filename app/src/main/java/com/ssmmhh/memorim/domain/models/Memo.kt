ce063ec108ff1f259c405a1aa03335ebb4621f2f
mahdi <ssmmhh1382@gmail.com>
2021-10-28 09:45:28 +0330

    create 'Memo' model

---
+++ app/src/main/java/com/ssmmhh/memorim/domain/models/Memo.kt
@@ -0,0 +1,11 @@
+package com.ssmmhh.memorim.domain.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Memo(
+    val id: Int,
+    val title: String,
+    val description: String
+) : Parcelable
