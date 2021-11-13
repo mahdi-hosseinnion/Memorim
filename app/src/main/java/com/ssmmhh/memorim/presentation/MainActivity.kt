42de0d7b4731bdb37a3eb064c5bf000dced16067
mahdi <ssmmhh1382@gmail.com>
2021-11-13 23:52:07 +0330

    textField

--- app/src/main/java/com/ssmmhh/memorim/presentation/MainActivity.kt
+++ app/src/main/java/com/ssmmhh/memorim/presentation/MainActivity.kt
@@ -6,15 +6,21 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ssmmhh.memorim.presentation.components.MemoCard
 import com.ssmmhh.memorim.presentation.ui.memo.MemoViewModel
 import com.ssmmhh.memorim.presentation.ui.memolist.MemoListViewModel
@@ -42,19 +48,25 @@ class MainActivity : ComponentActivity() {
             MemorimTheme {
                 // A surface container using the 'background' color from the theme
                 val greyBackgroundColor = Color(235, 236, 240)
-                Surface(color =greyBackgroundColor ) {
-                    LazyColumn {
-                        itemsIndexed(
-                            items = composeMemos
-                        ) { i, memo ->
-                            MemoCard(memo = memo, onClick = {
-                                Toast.makeText(
-                                    baseContext,
-                                    "Hey you clicked on memo with title: " + it.title,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            })
+                Surface(color = greyBackgroundColor) {
+                    Column {
+
+                        LazyColumn {
+                            itemsIndexed(
+                                items = composeMemos
+                            ) { i, memo ->
+                                MemoCard(memo = memo, onClick = {
+                                    Toast.makeText(
+                                        baseContext,
+                                        "Hey you clicked on memo with title: " + it.title,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                })
+                            }
                         }
+//                        Spacer(modifier = Modifier.padding(4.dp))
+                        TextField(value = "hey im text field", onValueChange = {})
+
                     }
                 }
             }
