781a34081538a3c8de08320f6da522d3793e0e98
mahdi <ssmmhh1382@gmail.com>
2021-11-24 19:37:55 +0330

    setup MemoListFragment basic ui

--- app/src/main/java/com/ssmmhh/memorim/presentation/ui/memolist/MemoListFragment.kt
+++ app/src/main/java/com/ssmmhh/memorim/presentation/ui/memolist/MemoListFragment.kt
@@ -10,11 +10,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,10 +21,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.ssmmhh.memorim.R
 import com.ssmmhh.memorim.presentation.components.MemoCard
 import com.ssmmhh.memorim.presentation.ui.theme.MemorimTheme
 import com.ssmmhh.memorim.presentation.ui.theme.Purple200
@@ -47,6 +48,12 @@ class MemoListFragment : Fragment() {
         }
     }
 
+    @Preview
+    @Composable
+    private fun PreviewMainCompose() {
+        MainCompose()
+    }
+
     @Composable
     private fun MainCompose() {
 
@@ -61,27 +68,47 @@ class MemoListFragment : Fragment() {
             // A surface container using the 'background' color from the theme
             val greyBackgroundColor = Color(235, 236, 240)
             Surface(color = greyBackgroundColor) {
-                Column {
+                Scaffold(
+                    topBar = {
+                        TopAppBar {
+                            Text(text = "Memorim", style = MaterialTheme.typography.h5)
 
-                    LazyColumn(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        itemsIndexed(
-                            items = composeMemos
-                        ) { i, memo ->
-                            MemoCard(memo = memo, onClick = {
-                                Toast.makeText(
-                                    requireContext(),
-                                    "Hey you clicked on memo with title: " + it.title,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            })
+                        }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = { onClickedOnAddFab() }) {
+                            Icon(Icons.Filled.Add, "Add new memo")
+                        }
+                    },
+                ) {
+                    Surface(color = greyBackgroundColor) {
+
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .fillMaxWidth()
+                        ) {
+                            itemsIndexed(
+                                items = composeMemos
+                            ) { i, memo ->
+                                MemoCard(memo = memo, onClick = {
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Hey you clicked on memo with title: " + it.title,
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                })
+                            }
                         }
                     }
                 }
             }
         }
 
+    }
+
+    private fun onClickedOnAddFab() {
+        findNavController().navigate(R.id.action_memoListFragment_to_memoFragment)
     }
 
 }
